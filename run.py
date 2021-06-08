3a27b8dd71ecf0ee5ad83444cb3e34edb4a9278a
parvin <bagirovp@gmail.com>
2021-06-08 18:07:07 +0400

    ilk task

---
+++ run.py
@@ -0,0 +1,19 @@
+# y="azerbaijan"
+# print(y)
+
+# print("bakugencesumgaitguba")
+# print("baku", "gence","sumgayit","guba", sep = '*')
+
+# a = "Macaristan"[::-1]
+# print(a)
+
+# print("Macaristan"[::-1])
+
+# print( " \"Forever for now\" is one of the. ")
+
+# a =  " \"Forever for now\" is one of the. "
+# print(a)
+
+phrase = "O, ümumi PFA və FWA mükafatını alan ilk futbolçu oldu. 2008,2013,2014,2016-cü illərdə Qızıl top mükafatını qazandı."
+first_half = phrase[0: len(phrase) // 2]
+print(first_half)
