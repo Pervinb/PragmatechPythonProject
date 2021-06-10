743e65d895b85b90996e3fee5cb4d6faca248a58
parvin <bagirovp@gmail.com>
2021-06-10 20:10:48 +0400

    ikinci

--- task_3.py
+++ task_3.py
@@ -1,3 +1,9 @@
+#  1) Write a Python function to sum all the numbers in a list.
+
+
+#  Sample List : (8, 2, 3, 0, 7) Expected Output : 20 
+
+ 
 list = [8, 2, 3, 0, 7]
 
 def summ(num):
@@ -10,6 +16,23 @@ def summ(num):
 
     return sum
 
-x = summ(list)
+
+print(summ(list))
+
+
+list = [8, 2, 3, -1, 7]
+
+def multiply(num):
+    
+    a= 1
+
+    for i in num:
+
+         a*= i
+
+    return a
+
+x = multiply(list)
 print(x)
+
 
