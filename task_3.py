5bc469fab75a495d71be5eb061071786fe7d6ec3
parvin <bagirovp@gmail.com>
2021-06-11 18:52:32 +0400

    yeni

--- task_3.py
+++ task_3.py
@@ -53,4 +53,31 @@ def get_value(i):
 my_dict ={1:"Monday", 2:"Tuesday", 3:"Wednesday", 4:"Thursday", 5:"Friday", 6:"Satuday", 7:"Sunday"}
 print(get_value(1))
 print(get_value(2))
-        
+
+# 4)Write a function called lastElement. This function takes one parameter (a list) and returns the last value in the list.
+#  It should return None if the list is empty.
+
+# Example Output lastElement([1,2,3]) # 3 lastElement([]) # None
+
+
+
+def list_test (list1):
+    if not list1      :
+         print('list is empty')
+    else: 
+        print(list1[-1])
+
+
+list_test([])
+list_test([1,2,3])
+
+# 5)Write a Python program to print the even numbers from a given list. 
+
+# Sample List : [1, 2, 3, 4, 5, 6, 7, 8, 9] Expected Result : [2, 4, 6, 8]  
+ 
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for num in list1:
+   if num % 2 == 0:
+      print(num, end = " ")
+
+
