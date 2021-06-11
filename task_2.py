45cce2bd7ac114e85663da50df67015018bc568e
parvin <bagirovp@gmail.com>
2021-06-12 02:46:33 +0400

    2ci

--- task_2.py
+++ task_2.py
@@ -1,13 +1,16 @@
 # 1)Kvadrat yaratmaq olarsa daxil edilən 4 ədəddən, ekrana kvadratın sahəsini, olmazsa, həmin ədədlərin cəmini verin.
+# 1
+
+
 a = int(input("Enter a:"))
 b = int(input("Enter b:"))
 c = int(input("Enter c:"))
 d = int(input("Enter d:"))
-h = a*b*c*d
-q = a+b+c+d
-if a==b==c==d:
-    print(h)
-else:
-    print(q)
-
-
+if a>b and a>c and a>d:
+    print(a)
+elif b>a and b>c and b>d:   
+    print(b)
+elif c>a and c>b and c>d:   
+    print(c) 
+elif d>a and d>b and d>c:   
+    print(d)         
