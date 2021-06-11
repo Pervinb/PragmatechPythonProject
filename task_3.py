fef3a1cc2ade62b09e7c1a383ba36a8947fe5995
parvin <bagirovp@gmail.com>
2021-06-11 04:12:38 +0400

    3uncu

--- task_3.py
+++ task_3.py
@@ -41,35 +41,16 @@ print(x)
 # 3)Write a function called returnDay. This function takes in one parameter ( a number from 1-7) and returns the day of the week
 # #  ( 1 is Sunday, 2 is Monday etc.). If the number is less than 1 or greater than 7, the function should return None.
 
-# def get_value(i):
-#     for key, value in my_dict.items():
-#          if i == key:
-#              return value
+def get_value(i):
+    for key, value in my_dict.items():
+         if i == key:
+             return value
  
-#     return "key doesn't exist"
+    return "key doesn't exist"
  
 
  
-# my_dict ={1:"Monday", 2:"Tuesday", 3:"Wednesday", 4:"Thursday", 5:"Friday", 6:"Satuday", 7:"Sunday"}
-# print(get_value(1))
-# print(get_value(2))
-
-returnDay = {
-1: 'Monday',
-2: 'Tuesday',
-3: 'Wednesday',
-4: 'Thursday',
-5: 'Friday',
-6: 'Saturday',
-7: 'Sunday',
-
-}
- 
-def day(number):
-    try:
-        return returnDay[number]
-    except:
-        return "error"
-
-print(day(5))
+my_dict ={1:"Monday", 2:"Tuesday", 3:"Wednesday", 4:"Thursday", 5:"Friday", 6:"Satuday", 7:"Sunday"}
+print(get_value(1))
+print(get_value(2))
         
